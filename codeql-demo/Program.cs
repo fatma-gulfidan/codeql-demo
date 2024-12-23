26c5eab2bf45a1e2676a56c656a105072ed8b2e7
Fatma Gülfidan <92696695+fatma-gulfidan@users.noreply.github.com>
2024-12-23 23:31:21 +0100

    Update Program.cs

--- codeql-demo/Program.cs
+++ codeql-demo/Program.cs
@@ -1,12 +1,10 @@
+
 using System;
 
 class Program
 {
-    const string ApiKey = "sk_test_1234567890abcdef"; // Hardcoded Secret
-
     static void Main()
     {
-        Console.WriteLine("Bu program bir güvenlik açığı içeriyor!");
-        Console.WriteLine($"API Anahtarı: {ApiKey}"); // Hardcoded Secret Kullanımı
+        Console.WriteLine("Hello, CodeQL!");
     }
 }
